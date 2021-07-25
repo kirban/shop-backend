7fc72c4f0dcaa69f5ee4aba3d3afe81f82898238
Kirill Romanov <kirill_romanov2@epam.com>
2021-07-25 11:11:24 +0300

    feat: implement try/catch in getAllProducts service

--- product-service/src/functions/get-all-products/handler.ts
+++ product-service/src/functions/get-all-products/handler.ts
@@ -4,10 +4,14 @@ import schema from './schema';
 //@ts-ignore
 import products from '../../data/productsList.json';
 
-const getAllProducts: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  return formatJSONResponse({
-    products,
-  })
+const getAllProducts: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (_event, _context, callback) => {
+  try {
+    return formatJSONResponse({
+      ...products,
+    });
+  } catch (error) {
+    callback(null, { statusCode: 500, body: error.toString() })
+  }
 }
 
 export const main = middyfy(getAllProducts);
